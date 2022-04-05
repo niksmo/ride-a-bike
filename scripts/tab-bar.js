f149accff841cc552115dfdbbdda1dbef2ebbf9d
Николай Смолов <n.smolov@yandex.ru>
2022-04-05 22:59:24 +0300

    adds feat/tab-bar for bikes sliders without interaction

---
+++ scripts/tab-bar.js
@@ -0,0 +1,5 @@
+const buttons = document.querySelectorAll('.button');
+
+const buttonHighway = document.querySelector('.button_bike_highway');
+const buttonGravel = document.querySelector('.button_bike_gravel');
+const buttonTt = document.querySelector('.button_bike_tt');
