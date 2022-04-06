1bcc8e66a254bf94bb9829f0c6f0ff7c92be6323
Nikolay Smolov <n.smolov@yandex.ru>
2022-04-06 20:22:26 +0300

    adds feat/tab-bar interraction

--- scripts/filter.js
+++ scripts/filter.js
@@ -2,14 +2,20 @@ const filter = document.querySelector('.filter');
 const accordion = filter.querySelector('.filter__current');
 const popup = filter.querySelector('.filter__popup');
 
+const filterItems = filter.querySelectorAll('.filter__popup-item');
 const filterHighway = filter.querySelector('.filter__popup-item_bike_highway');
 const filterGravel = filter.querySelector('.filter__popup-item_bike_gravel');
 const filterTt = filter.querySelector('.filter__popup-item_bike_tt');
 
+const buttons = document.querySelectorAll('.button');
+const buttonHighway = document.querySelector('.button_bike_highway');
+const buttonGravel = document.querySelector('.button_bike_gravel');
+const buttonTt = document.querySelector('.button_bike_tt');
+
 const bikeSliders = document.querySelectorAll('.bikes-slider');
-const sliderHighway = document.querySelector('.bikes-slider_type_highway');
-const sliderGravel = document.querySelector('.bikes-slider_type_gravel');
-const sliderTt = document.querySelector('.bikes-slider_type_tt');
+const bikesHighway = document.querySelector('.bikes-slider_type_highway');
+const bikesrGravel = document.querySelector('.bikes-slider_type_gravel');
+const bikesTt = document.querySelector('.bikes-slider_type_tt');
 
 
 function toggleAccordion () {
@@ -17,7 +23,7 @@ function toggleAccordion () {
   popup.classList.toggle('filter__popup_hidden');
 }
 
-accordion.addEventListener('click', toggleAccordion, false);
+
 
 function hiddeSliders (array) {
   array.forEach(function (item) {
@@ -27,25 +33,66 @@ function hiddeSliders (array) {
   })
 }
 
-function showSlider (filterItem) {
-  if (filterItem === filterHighway && sliderHighway.classList.contains('bikes-slider_hidden')) {
+function showSlider (selectedItem) {
+  if (selectedItem === filterHighway && bikesHighway.classList.contains('bikes-slider_hidden')) {
     hiddeSliders (bikeSliders);
-    sliderHighway.classList.toggle('bikes-slider_hidden');
-  } else if (filterItem === filterGravel && sliderGravel.classList.contains('bikes-slider_hidden')) {
+    toggleBikes(bikesHighway);
+  } else if (selectedItem === filterGravel && bikesrGravel.classList.contains('bikes-slider_hidden')) {
     hiddeSliders (bikeSliders);
-    sliderGravel.classList.toggle('bikes-slider_hidden');
-  } else if (filterItem === filterTt && sliderTt.classList.contains('bikes-slider_hidden')) {
+    toggleBikes(bikesrGravel);
+  } else if (selectedItem === filterTt && bikesTt.classList.contains('bikes-slider_hidden')) {
     hiddeSliders (bikeSliders);
-    sliderTt.classList.toggle('bikes-slider_hidden');
+    toggleBikes(bikesTt);
   }
 }
 
 function applyFilter () {
-  accordion.textContent = this.firstChild.textContent;
+  const selectedItem = this;
+
+  accordion.textContent = selectedItem.firstChild.textContent;
   toggleAccordion ();
-  showSlider(this);
+  showSlider(selectedItem);
 }
 
-filterHighway.addEventListener('click', applyFilter);
-filterGravel.addEventListener('click', applyFilter);
-filterTt.addEventListener('click', applyFilter);
+accordion.addEventListener('click', toggleAccordion, false);
+
+filterItems.forEach((item) => item.addEventListener('click', applyFilter));
+
+
+function toggleTab(el) {
+  if (!el.classList.contains('button_active')) {
+    buttons.forEach(function (item) {
+      if (item.classList.contains('button_active')) {
+        item.classList.remove('button_active');
+      }
+    })
+    el.classList.toggle('button_active');
+  } else {
+    return;
+  }
+}
+
+function toggleBikes(bikeType) {
+  bikeType.classList.toggle('bikes-slider_hidden');
+};
+
+
+function chooseBikes(evt) {
+  const selectedButton = evt.target;
+
+  if (selectedButton === buttonHighway && bikesHighway.classList.contains('bikes-slider_hidden')) {
+    toggleTab(selectedButton);
+    hiddeSliders(bikeSliders);
+    toggleBikes(bikesHighway);
+  } else if (selectedButton === buttonGravel && bikesrGravel.classList.contains('bikes-slider_hidden')) {
+    toggleTab(selectedButton);
+    hiddeSliders(bikeSliders);
+    toggleBikes(bikesrGravel);
+  } else if (selectedButton === buttonTt && bikesTt.classList.contains('bikes-slider_hidden')) {
+    toggleTab(selectedButton);
+    hiddeSliders(bikeSliders);
+    toggleBikes(bikesTt);
+  }
+}
+
+buttons.forEach((item) => item.addEventListener('click', chooseBikes));
--- scripts/tab-bar.js
+++
@@ -1,5 +0,0 @@
-const buttons = document.querySelectorAll('.button');
-
-const buttonHighway = document.querySelector('.button_bike_highway');
-const buttonGravel = document.querySelector('.button_bike_gravel');
-const buttonTt = document.querySelector('.button_bike_tt');
